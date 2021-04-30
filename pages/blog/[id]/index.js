b9e98d9ad1a6566d92a5d05bc52413e42a782e81
therealrinku <backupfb2032@gmail.com>
2021-04-30 14:26:11 +0545

    styles added

--- pages/blog/[id]/index.js
+++ pages/blog/[id]/index.js
@@ -1,10 +1,30 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import db from "../../../firebase/db";
+import blogStyles from "../../../styles/blog.module.css";
 
 const Blog = () => {
+  const [details, setDetails] = useState({});
+
   const router = useRouter();
   const { id } = router.query;
 
-  return <div style={{ width: "75%", margin: "auto", marginTop: "100px" }}>{id}</div>;
+  useEffect(() => {}, [
+    db
+      .collection("blogs")
+      .doc(id)
+      .get()
+      .then((doc) => {
+        setDetails(doc.data());
+      }),
+  ]);
+
+  return (
+    <div className={blogStyles.Blog}>
+      <h2>{details.title}</h2>
+      <p>{details.content}</p>
+    </div>
+  );
 };
 
 export default Blog;
